40f2738b9cf3d5f1a3236042bf8749ded61dbfb7
bxc1712 <bxc1712@rit.edu>
2017-05-16 11:12:02 -0400

    Combined Files

---
+++ My Grape Vine/My Grape Vine/WineAppData.swift
@@ -0,0 +1,20 @@
+//
+//  WineAppData.swift
+//  My Grape Vine
+//
+//  Created by Daniel Martin (RIT Student) on 4/26/17.
+//  Copyright © 2017 Daniel Martin (RIT Student). All rights reserved.
+//
+
+import Foundation
+
+class WineData {
+    static let sharedData = WineData()
+    
+    var sortBy = [
+        "Popularity","Rating"
+    ]
+
+    private init(){
+    }
+}
